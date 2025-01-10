b66d06beba11177e7a9e785308b1e4630fc62289
Abhisekh kumar singh <abhisekh.kumar-singh@group.one>
2025-01-10 21:46:38 +0530

    [golang] backend pubsub for chat application

---
+++ backend/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/websocket"
+	"github.com/redis/go-redis/v9"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var connections = make(map[string]*websocket.Conn)
+var message struct {
+	Type    string `json:"type"`
+	Message struct {
+		Sender   string `json:"sender"`
+		Receiver string `json:"receiver"`
+		Text     string `json:"text"`
+	} `json:"message"`
+}
+
+func main() {
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Println("Error getting hostname:", err)
+	}
+	fmt.Println("Hostname:", hostname)
+
+	opt, err := redis.ParseURL("redis://:webmail@localhost:6379/")
+	if err != nil {
+		panic(err)
+	}
+	client := redis.NewClient(opt)
+	ctx := context.Background()
+
+	go func() {
+		pubsub := client.Subscribe(ctx, "hostname:"+hostname)
+		_, err = pubsub.Receive(ctx)
+		if err != nil {
+			panic(err)
+		}
+		ch := pubsub.Channel()
+		for msg := range ch {
+			var receivedMessage struct {
+				Type    string `json:"type"`
+				Message struct {
+					Sender   string `json:"sender"`
+					Receiver string `json:"receiver"`
+					Text     string `json:"text"`
+				} `json:"message"`
+			}
+			if err := json.Unmarshal([]byte(msg.Payload), &receivedMessage); err != nil {
+				log.Println("Error unmarshalling message:", err)
+				continue
+			}
+			if receivedMessage.Type == "message" {
+				if receiverConn, ok := connections[receivedMessage.Message.Receiver]; ok {
+					if err := receiverConn.WriteMessage(websocket.TextMessage, []byte(msg.Payload)); err != nil {
+						log.Println("Error sending message to receiver:", err)
+					}
+				} else {
+					log.Println("Receiver not connected:", receivedMessage.Message.Receiver)
+				}
+			}
+		}
+	}()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		user_id := r.URL.Query().Get("user")
+		connections[user_id] = conn // how to keep track of multiple connections of a same user from multiple devices?
+
+		err = client.Set(ctx, "user:"+user_id, hostname, 0).Err()
+		if err != nil {
+			log.Println("Error setting user:", err)
+			return
+		}
+		log.Printf("User %s connected to %s ", user_id, hostname)
+		// defer func() {
+		// 	conn.Close()
+		// 	delete(connections, user_id)
+		// 	log.Println("User disconnected:", user_id)
+		// }()
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				break
+			}
+
+			if err := json.Unmarshal(msg, &message); err != nil {
+				log.Println("Error unmarshalling message:", err)
+				continue
+			}
+			if message.Type == "message" {
+				if receiverConn, ok := connections[message.Message.Receiver]; ok {
+					if err := receiverConn.WriteMessage(websocket.TextMessage, msg); err != nil {
+						log.Println("Error sending message to receiver:", err)
+					}
+					continue
+				}
+
+				host, err := client.Get(ctx, "user:"+message.Message.Receiver).Result()
+				if err != nil {
+					log.Println("Error getting user:", err)
+					continue
+				}
+				// Publish message to Redis channel
+				if err = client.Publish(ctx, "hostname:"+host, msg).Err(); err != nil {
+					log.Println("Error publishing message:", err)
+				} else {
+					log.Println("Message published by user:", user_id)
+				}
+				// } else {
+				// 	log.Println("Receiver not connected:", message.Message.Receiver)
+				// }
+			}
+		}
+	})
+	http.ListenAndServe(":8080", nil)
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,41 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-
+const usersList = [
+  { name: 'User 1', id: "user_1", online: Math.random() > 0.5 },
+  { name: 'User 2', id: "user_2", online: Math.random() > 0.5 },
+  { name: 'User 3', id: "user_3", online: Math.random() > 0.5 },
+  { name: 'User 4', id: "user_4", online: Math.random() > 0.5 }
+];
 function App() {
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState('');
-  const [user] = useState(`User${Math.floor(Math.random() * 100)}`);
-  const [users, setUsers] = useState([
-    { name: 'User1', online: Math.random() > 0.5 },
-    { name: 'User2', online: Math.random() > 0.5 }
-  ]);
+  // Randomly select a user as a loggedin error
+  const [user] = useState(
+    usersList[Math.floor(Math.random() * usersList.length)]
+  );
+  const users = usersList.filter((u) => u.id !== user.id);
   const [selectedUser, setSelectedUser] = useState(null); // State for selected user
   const ws = useRef(null);
 
   useEffect(() => {
-    ws.current = new WebSocket('ws://localhost:8080/ws?user=' + user);
+    ws.current = new WebSocket('ws://192.168.31.212:8080/ws?user=' + user.id);
     ws.current.onopen = () => console.log('Connected to WebSocket');
     ws.current.onmessage = (event) => {
       const data = JSON.parse(event.data);
-      if (data.type === 'message') {
-        setMessages((prevMessages) => [...prevMessages, data.message]);
-      } else if (data.type === 'users') {
-        setUsers(data.users);
-      }
+      setMessages((prevMessages) => [...prevMessages, data.message]);
+    };
+    ws.current.onclose = () => {
+      console.log('WebSocket disconnected');
+      // setTimeout(connectWebSocket, 1000); // Reconnect after 1 second
     };
     return () => {
       ws.current.close();
     };
-  }, [user]);
+  }, [user.id]);
 
   const handleSendMessage = () => {
     if (inputValue.trim() && selectedUser) {
-      const message = { sender: user, receiver: selectedUser.name, text: inputValue };
+      const message = { sender: user.id, receiver: selectedUser.id, text: inputValue };
       ws.current.send(JSON.stringify({ type: 'message', message }));
       setMessages((prevMessages) => [...prevMessages, message]);
       setInputValue('');
@@ -44,12 +49,12 @@ function App() {
   return (
     <div className="App">
       <div className="user-list">
-        <h3>Users</h3>
+        <h3>Online Users</h3>
         <ul>
           {users.map((user, index) => (
             <li
               key={index}
-              className={`${user.online ? 'online' : 'offline'} ${selectedUser && selectedUser.name === user.name ? 'selected' : ''}`}
+              className={`${user.online ? 'online' : 'offline'} ${selectedUser && selectedUser.id === user.id ? 'selected' : ''}`}
               onClick={() => handleUserClick(user)}
             >
               {user.name}
@@ -63,7 +68,7 @@ function App() {
           <div className="messages">
             {messages
               .map((message, index) => (
-                <div key={index} className={`message ${message.sender === user ? 'sent' : 'received'}`}>
+                <div key={index} className={`message ${message.sender === user.id ? 'sent' : 'received'}`}>
                   <div className="message-info">
                     <span className="message-sender">{message.sender}</span>
                     <div className="message-text">{message.text}</div>
@@ -87,6 +92,12 @@ function App() {
           </div>
         </div>
       )}
+        <div className="user-list loggenId">
+        <h3>loggedIn as:</h3>
+        <ul>
+          <li>{user.name}</li>
+        </ul>
+      </div>
     </div>
   );
 }
